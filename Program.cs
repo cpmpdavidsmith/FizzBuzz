e32e531319c13a83f9f0a147147b4dcc156e2523
David Smith <cpmpdavidsmith@icloud.com>
2023-03-02 16:41:53 -0500

    FizzBuzz

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+using System.IO;
+namespace WhiteBoard
+{
+    class Program
+    {
+        public string FizzBuzzNumber(int number)
+        {
+            //if (number % 3 == 0 && number % 5 == 0)
+            //{
+            //    return "fizzbuzz";
+            //}
+            //if (number % 3 == 0)
+            //{
+            //    return "fizz";
+            //}
+            //if (number % 5 == 0)
+            //{
+            //    return "buzz";
+            //}
+            var answer = "";
+            if(number % 3 == 0)
+            {
+                answer = "fizz";
+            }
+            if(number % 5 == 0)
+            {
+                answer += "buzz";
+            }
+            return answer;
+        }
+    }
+    
+}
+
+
+
+
